a58271a1f2f6025a2a8209fd08d8716b6882b99c
Vrund Padariya <vspadariya@myseneca.ca>
2025-05-22 12:46:09 -0400

    Individual message or note.

---
+++ lab2e.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#Name:Vrund Padariya
+#AuthorID:vspadariya
+#Date Created:2025/05/20
+timer = 10
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
